978ab495d8b3e44c8402a9b9f36926322475775e
Penple03 <Penple03@users.noreply.github.com>
2018-08-02 21:26:09 -0700

    Add component augmentation to Button

--- react/Button.jsx
+++ react/Button.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import {pick} from '../lib/utils'
 
-export default class Button extends React.Component {
-  render () {
-    const {children, ...mods} = this.props
-    return <button block='b' mods={mods}>{children}</button>
-  }
+const Button = () => {
+  const mods = pick(this.props, ['primary', 'secondary', 'disabled', 'outline', 'text'])
+  const {children, as, primary, secondary, disabled, outline, text, ...rest} = this.props
+  const As = as || 'button'
+  return <As block='b' mods={mods} {...rest}>{children}</As>
 }
+
+export default Button
